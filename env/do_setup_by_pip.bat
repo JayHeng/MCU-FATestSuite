94d13ef86036c7d4144d232d4fd0ccada0854ae9
Jay Heng <hengjie1989@foxmail.com>
2026-01-14 22:14:54 +0800

    Update pack bat
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- env/do_setup_by_pip.bat
+++ env/do_setup_by_pip.bat
@@ -4,5 +4,8 @@ pip.exe install pylink-square==1.2.0
 pip.exe install pyserial==3.5
 pip.exe install bincopy==20.0.0
 pip.exe install openpyxl==3.1.5
+pip.exe install opencv-python==4.11.0.86
+pip.exe install pytesseract==0.3.13
+pip.exe install matplotlib==3.10.0
 
 pip.exe install PyInstaller==6.4.0
